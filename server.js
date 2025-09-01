0aa558bf8cb9a4e7d2376f34489898bbd7e57f0e
inkkaripokeri <villedot@gmail.com>
2025-09-01 22:14:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const wss = new WebSocket.Server({ port });
 wss.on('connection', ws => {
   console.log('Yhteys avattu');
 
-  ws.on('message', msg => {
-    console.log('Vastaanotettu:', msg);
-    ws.send(`Sait viestin: ${msg}`);
+  ws.on('message', message => {
+    // Muutetaan Buffer → string
+    const msgText = message.toString();
+    console.log('Vastaanotettu:', msgText);
+
+    // Lähetetään Origamille
+    ws.send(JSON.stringify({ reply: `Sait viestin: ${msgText}` }));
   });
 
-  ws.send('Tervetuloa WebSocket-serveriin!');
+  // Lähetetään heti tervehdys JSON-muodossa
+  ws.send(JSON.stringify({ msg: 'Tervetuloa WebSocket-serveriin!' }));
 });
 
 console.log(`WebSocket-serveri käynnissä portissa ${port}`);
